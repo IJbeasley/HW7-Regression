83bb26f72edd772e956a302a7a2d4de945691caf
Isobel Beasley <58153806+IJbeasley@users.noreply.github.com>
2025-02-24 18:23:08 -0800

    Update test_logreg.py for loss_function unit test

--- test/test_logreg.py
+++ test/test_logreg.py
@@ -10,13 +10,49 @@ This is not an exhaustive list.
 
 # Imports
 import pytest
+import numpy as np
+
+# Our regression module
+from regression import logreg, utils
+
+# Use scikit-learn to check the correctness of our model
+from sklearn.metrics import log_loss
+
 # (you will probably need to import more things here)
 
 def test_prediction():
 	pass
 
 def test_loss_function():
-	pass
+	"""
+        Unit test to check that loss estimated by the regression module's loss_function is being calculated correctly,
+	by comparing it to scikit learn's log_loss function using true y values from data/nsclc.csv, and randomly generated y predictions. 
+        """
+
+	# Load only y_train as our y_true - X is not needed for this test
+         _, _, y_true, _ = utils.loadDataset(
+                features=[
+                  'Penicillin V Potassium 500 MG',
+	          'Computed tomography of chest and abdomen',
+		],
+                   split_percent=0.8,
+                   split_seed=42
+                                                  )
+
+        # Create a y_pred array, of random 0s and 1s
+        # Set seed for reproducibility
+        np.random.seed(42)
+        y_pred = np.random.randint(0, 2, size=y_true.shape)
+
+        # Calculate loss using loss_function from regression module
+        lr_mod = LogisticRegressor(num_feats = 2)
+        lr_mod.loss_function(y_true, y_pred)
+
+        # Calculate loss using scikit learn's log_loss function
+        true_loss=log_loss(y_true, y_pred)
+
+        assert np.isclose(logreg_loss, sklearn_loss), 'Calculated loss by loss_function is not correct'
+
 
 def test_gradient():
 	pass
